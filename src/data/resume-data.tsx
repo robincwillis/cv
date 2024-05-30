55e7f039066e521f220432311ec4401b60c10c45
robincwillis <robincwillis@gmail.com>
2024-05-30 13:50:44 -0700

    update content

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -30,7 +30,7 @@ export const RESUME_DATA = {
    about:
      "Product army of one and full stack developer advancing and challenging the boundaries of both disciplines",
   summary:
-    "2x founder, seasoned technology and design leader with over 14 years of experience, I excel at transforming complex problems into successful products at intersection of design and engineering.",
+    "Seasoned technology and design leader with over 14 years of experience spanning from UX UI design & research to full-stack development. I excel in discovering opportunities, validating their potential, and transforming abstract ideas into innovative products that deliver impactful solutions.",
   avatarUrl: Avatar,
   personalWebsiteUrl: "https://robincwillis.com",
   contact: {
@@ -57,7 +57,7 @@ export const RESUME_DATA = {
   education: [
     {
       school: "Massachusetts Institute of Technology",
-      degree: "MARCH, Architecture",
+      degree: "MARCH, Masters Architecture",
       start: "2007",
       end: "2011",
     },
@@ -72,63 +72,74 @@ export const RESUME_DATA = {
     {
       company: "Tender",
       link: "https://tenderco.co",
-      badges: ["founder", "design", "engineering", "leadership"],
+      badges: ["founder", 'research', "design", "engineering", "leadership"],
       title: "Head of Product Design",
       start: "2022",
       end: "2024",
       description:
-      `Leading the charge in revolutionizing caregiving through innovative technology. My role involves crafting a forward-thinking product vision, guiding the roadmap, and orchestrating all facets of product design and development. Key achievements include spearheading transformative features, cultivating a culture of innovation, and building a cohesive, efficient product team. I prioritize a user-centric approach, resulting in increased engagement, satisfaction, and retention, contributing to Tender's mission of creating a seamless caregiving platform.`
+      `Leading the charge in Tender's mission of creating a seamless caregiving platform. I orchestrate the forward-thinking product vision, strategy, and execution, while contributing directly to the research, design and development process, embodying an empathetic approach to product creation.`
+      //`Leading the charge in revolutionizing caregiving through innovative technology. My role involves crafting a forward-thinking product vision, guiding the roadmap, and orchestrating all facets of product design and development. Key achievements include spearheading transformative features, cultivating a culture of innovation, and building a cohesive, efficient product team. I prioritize a user-centric approach, resulting in increased engagement, satisfaction, and retention, contributing to Tender's mission of creating a seamless caregiving platform.`
       //`Orchestrated caregiving innovation through a forward-thinking product vision. Led transformative features and built an efficient product team.`
     },
     {
       company: "Gordils & Willis",
       link: "https://gordilsandwillis.com",
-      badges: ["founder", "design", "engineering", "leadership"],
+      badges: ["founder", 'research', "design", "engineering", "leadership"],
       title: "Partner",
       start: "2017",
       end: "2022",
       description:
-        `Led company-wide strategic vision, management, and day-to-day operations to ensure high-quality work delivery. Took a hands-on approach in both design and engineering, contributing to client projects and staying current with the latest industry techniques. Conducted regular design and engineering reviews for quality assurance, while managing and mentoring team members. Cultivated strong client relationships, understanding their needs to deliver exceptional service and results.`
+        `Led company-wide strategic vision and operations to bring a number of startups to market and transition into sustainable businesses through a hands-on approach across design and engineering. Managed and mentored team members, cultivated strong client relationships to deliver exceptional service and results.`
+        //`Led company-wide strategic vision, management, and day-to-day operations to ensure high-quality work delivery. Took a hands-on approach in both design and engineering, contributing to client projects and staying current with the latest industry techniques. Conducted regular design and engineering reviews for quality assurance, while managing and mentoring team members. Cultivated strong client relationships, understanding their needs to deliver exceptional service and results.`
         //`Directed company vision, ensuring high-quality work. Maintained hands-on design and engineering oversight, fostering client relationships for exceptional service and results.`
     },
     {
       company: "CompStak",
       link: "https://compstak.com/",
-      badges: ['design', 'engineering', 'management'],
+      badges: ['research', 'design', 'engineering', 'management'],
       title: "Product Designer / Software Engineer",
       start: "2014",
       end: "2017",
       description:
-        `Led UI/UX design and research. Spearheaded redesign and front-end overhaul– enhancing usability and improving sales and user retention.`
+        `Led a complete product redesign and front-end framework overhaul by identifying UX UI problems and opportunities, and refining design concepts through rigorous research and validation. Leading to a significant impact in usability, retention and conversion. Additionally, designed and developed internal tools that improved the company's sales cycle, and productivity.`
+        //`Led UI/UX design and research. Spearheaded redesign and front-end overhaul– enhancing usability and improving sales and user retention.`
     },
     {
       company: "Sunglass",
       link: undefined,
-      badges: ['design', 'engineering', 'management'],
+      badges: ['research', 'design', 'engineering', 'management'],
       title: "Software Engineer",
       start: "2012",
       end: "2013",
       description:
-       `Designed and developed multiple 3D design application plug-ins, seamlessly interacting with a web API through various languages and interfaces. Additionally, I created the companion iOS App, enabling users to download, navigate, and add annotations, photos, and comments onto 3D models. Managed external development through contractors and effectively coordinated efforts with the internal development team for seamless integration.`
-       // `Designed and developed 3D design application plug-ins, managing external contractors. Created a companion iOS App.`
+        `Designed and developed multiple application plug-ins, seamlessly interacting with a Sunglass’s application through various languages and interfaces. Additionally, I created a companion iOS App. Managed external development through contractors and effectively coordinated efforts with the internal development team for seamless integration.`
+        // `Designed and developed multiple 3D design application plug-ins, seamlessly interacting with a web API through various languages and interfaces. Additionally, I created the companion iOS App, enabling users to download, navigate, and add annotations, photos, and comments onto 3D models. Managed external development through contractors and effectively coordinated efforts with the internal development team for seamless integration.`
+        // `Designed and developed 3D design application plug-ins, managing external contractors. Created a companion iOS App.`
     }
   ],
   skills: [
-    "JavaScript/TypeScript",
-    "React/Next.js",
-    "Node.js",
-    "GraphQL",
-    "React Native",
-    "Swift",
-    "Python",
-    "CSS/SCSS/Tailwind",
-    "WebGL/Three.js",
-    "Figma/Adobe",
+    "Product Design",
+    "Mobile & Web Development",
+    "UX Research & Insights",
+    "Creative Direction",
+    "Product Strategy",
+    "Systems Architecture",
+    "Brand Design & Strategy",
+    "Figma, Adobe Creative Suite",
     "User Interface Design",
     "Data Visualization",
     "3D Modeling",
     "User Experience Research",
-    "Project Management/Agile"
+    "JavaScript/TypeScript",
+    "React.js, React Native",
+    "Next.js,  Node.js",
+    "GraphQL, Apollo",
+    "React Native",
+    "Framer, Origami",
+    "Python, Ruby, Rails",
+    "HTML, CSS, SCSS, Tailwind",
+    "OpenGL, WebGL, Three.js",
+    "Project Management, Agile Methodologies"
   ],
   projects: [
     // {
